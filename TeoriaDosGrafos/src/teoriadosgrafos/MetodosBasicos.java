d8bd9b034ef09d1426eafd8df1dd57f831d33670
Diego <diego_alessandro@hotmail.com>
2020-10-15 10:09:36 -0300

    Adiciona os metodos EhRegular e GetAdjacentes

--- TeoriaDosGrafos/src/teoriadosgrafos/MetodosBasicos.java
+++ TeoriaDosGrafos/src/teoriadosgrafos/MetodosBasicos.java
@@ -5,7 +5,9 @@
  */
 package teoriadosgrafos;
 
+import java.util.Arrays;
 import java.util.LinkedList;
+import java.util.List;
 
 /**
  *
@@ -26,4 +28,64 @@ public class MetodosBasicos {
         }
         return false;
     } 
+    static void getAdjacentes(List[] grafo, int vertice){
+        
+        int contador=0;
+        for(int i =0; i<grafo.length;i++){
+            
+            if (i==vertice){
+            System.out.println("Vértices adjacentes ao vértice "+i+":");
+              for (int j = 0;j<grafo[i].size();j++){
+                  
+                  contador = contador +1;
+                  
+                  System.out.println(grafo[i].get(j)+ " ");}
+                    
+             
+            System.out.println("O número de vértices adjacentes é:"+ contador);
+        
+    }
+}
+
+    
+
+    }
+     static boolean ehRegular(List[] grafo){
+        int contador=0;
+        int aux=0;
+        int[] vetor = new int[12];
+        //descobre o número de adjacentes em cada vértice e aloca esse número em uma posição do vetor
+        for(int i =0; i<grafo.length;i++){
+            
+            for (int j = 0;j<grafo[i].size();j++){
+                  
+                  contador = contador +1;
+                  
+            }   
+           vetor[i]=contador;
+           contador = 0;
+            }
+        // compara os valores do vetor, caso sejam diferentes, muda-se o valor da variável auxiliar
+        for(int i =0;i<vetor.length;i++){
+            if (vetor[i]!=vetor[i+1]){
+                aux++;
+                break;
+             }
+            else{
+                aux=0;
+            }
+        }
+             
+    
+    if(aux!=0){
+        
+        
+        return false;
+    }    
+    else{
+        
+        return true;
+    }
+    
+    }
 }
--- TeoriaDosGrafos/src/teoriadosgrafos/TeoriaDosGrafos.java
+++ TeoriaDosGrafos/src/teoriadosgrafos/TeoriaDosGrafos.java
@@ -9,11 +9,17 @@ public class TeoriaDosGrafos {
     public static void main(String[] args) {
         // Representação de um grafo
         Lista.RepresentarGrafoLista();
-        
+        var grafo = Lista.GerarGrafo();
         //Metodos basicos
+        MetodosBasicos.getAdjacentes(grafo,4);
+        MetodosBasicos.getAdjacentes(grafo,3);
+        MetodosBasicos.getAdjacentes(grafo,1);
+        System.out.println();
+        MetodosBasicos.ehRegular(grafo);
+        System.out.println("Esse grafo é regular? " + MetodosBasicos.ehRegular(grafo));
         //Verificar se um grafo é completo
         System.out.println("===========================");
-        var grafo = Lista.GerarGrafo();
+        
         System.out.println("Esse grafo é completo? " + MetodosBasicos.EhCompleto(grafo));
     }
     
