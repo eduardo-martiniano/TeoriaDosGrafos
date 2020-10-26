c6bb5c572741930d385948465fdcd1897df5df35
diegoabs84 <71224489+diegoabs84@users.noreply.github.com>
2020-10-26 12:39:27 -0300

    Update Dijkstra.java

--- TeoriaDosGrafos/src/teoriadosgrafos/Dijkstra.java
+++ TeoriaDosGrafos/src/teoriadosgrafos/Dijkstra.java
@@ -82,43 +82,7 @@ public class Dijkstra {
       
 
 	 
-	public static void main(String arg[]) 
-	{ 
-		int numVértices = 5; 
-		int origem = 0; 
-
-		 
-		// Lista de adjacência como representação 
-		List<List<Vértice> > adjacência = new ArrayList<List<Vértice> >(); 
-
-		// Inicia a lista para cada vértice 
-		for (int i = 0; i < numVértices; i++) { 
-			List<Vértice> item = new ArrayList<Vértice>(); 
-			adjacência.add(item); 
-		} 
-
-		// Alimentando o grafo 
-		adjacência.get(0).add(new Vértice(1, 5)); 
-		adjacência.get(0).add(new Vértice(2, 2)); 
-		adjacência.get(1).add(new Vértice(4, 10));
-                adjacência.get(1).add(new Vértice(3, 4));
-                adjacência.get(2).add(new Vértice(1, 4)); 
-		adjacência.get(2).add(new Vértice(3, 10));
-                adjacência.get(3).add(new Vértice(4, 6));
-                
-                
-                
-
-		// Calcula o menor caminho desde a origem 
-		Dijkstra D = new Dijkstra(numVértices); 
-		D.dijkstra(adjacência, origem); 
-
-		 
-		// Imprime o menor caminho para todos os outros vértices desde o vértice de origem estabelecido 
-		 
-		for (int i = 0; i < D.distância.length; i++){ 
-			System.out.println("O caminho mais curto desde o Vértice "+origem + " para " + i + " é "+ D.distância[i]); }
-	} 
+	
 } 
 
          
